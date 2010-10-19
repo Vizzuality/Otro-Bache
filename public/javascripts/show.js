86639ecfcb428b3e371a9d6069a9ae01c9cea5ca
javomorales <javomorales@gmail.com>
2010-10-19 18:10:52 +0200

    Add show.js

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -1,17 +1,31 @@
 var map;
+var FUSION_TABLES_ID = 272266;
 
 $(document).ready(function() { 
 	
-    var myLatlng = new google.maps.LatLng($("#lat").text(), $("#lon").text());
+    var mapLatLon = new google.maps.LatLng($("#lat").text(), $("#lon").text());
     var myOptions = {
-      zoom: 8,
-      center: myLatlng,
+      zoom: 14,
+      center: mapLatLon,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
 			disableDefaultUI: false,
 			scrollwheel: false
     }
-	geocoder = new google.maps.Geocoder();
     
+    var marker = new google.maps.Marker ({
+      map: myOptions,
+      position: new google.maps.LatLng($("#lat").text(), $("#lon").text()),
+      draggable: true,
+    });
+
+    var circle = new google.maps.Circle ({
+      map: myOptions,
+      radius: 1
+    });
+
+    circle.bindTo('center', marker, 'position')
+   
+    geocoder = new google.maps.Geocoder();
     
     map = new google.maps.Map(document.getElementById("main_map"), myOptions);
 	
